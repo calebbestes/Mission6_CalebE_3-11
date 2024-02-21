a8dcb257bf5d95c63f8c0d5904b7f2049288d3f9
calebestes <estes.caleb.b@gmail.com>
2024-02-21 14:54:49 -0700

    All

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,32 +6,52 @@ namespace Mission6_CalebE_3_11.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private MovieCollectionContext _context;
+        public HomeController(MovieCollectionContext temp)
         {
-            _logger = logger;
+            _context = temp;
         }
 
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Collection()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult AboutJoel()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Form()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult MovieCollection(Movies Response)
+        {
+            _context.Movies.Add(Response);
+            _context.SaveChanges();
+
+            return View("Confirmation", Response);
+        }
+
 
     }
 }
--- Models/Model.cs
+++ Models/Model.cs
@@ -7,24 +7,29 @@ namespace Mission6_CalebE_3_11.Models
         [Key]
         public int MovieID { get; set; }
 
+        [Required]
         public string Category { get; set; }
 
+        [Required]
         public string Title {  get; set; }
 
-        public int Year { get; set; }   
+        [Required]
+        public int Year { get; set; }
 
+        [Required]
         public string Director {  get; set; }
 
-
+        [Required]
         public string Rating { get; set; }  
+
 
         public bool Edited { get; set; }
 
 
-        public string Borrower { get; set; }    
+        public string? Borrower { get; set; }
 
-
-        public string Notes { get; set; }
+        [StringLength(25)]
+        public string? Notes { get; set; }
 
 
     }
